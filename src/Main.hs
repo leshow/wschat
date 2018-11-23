848be9ef6efd44294d3d4cd8e5d2846e74d9afe7
Evan Cameron <evan@vectorface.com>
2018-11-23 13:33:18 -0500

    use STM

--- src/Main.hs
+++ src/Main.hs
@@ -11,11 +11,12 @@ import           Control.Exception                   ( finally )
 import           Control.Monad                       ( forM_
                                                      , forever
                                                      )
-import           Control.Concurrent                  ( MVar
-                                                     , newMVar
-                                                     , modifyMVar_
-                                                     , modifyMVar
-                                                     , readMVar
+import           Control.Concurrent.STM              ( TVar
+                                                     , newTVar
+                                                     , modifyTVar'
+                                                     , readTVar
+                                                     , readTVarIO
+                                                     , atomically
                                                      )
 import           Control.Concurrent.Async            ( async )
 import qualified Control.Concurrent.Chan.Unagi      as UC
@@ -25,15 +26,15 @@ import qualified Network.Wai.Handler.WebSockets     as WaiWS
 import           ChatState
 
 -- WS.ServerApp is an alias for PendingConnection -> IO ()
-application :: UC.InChan Text -> MVar ServerState -> WS.ServerApp
-application logChan state pending = do
+application :: TVar ServerState -> WS.ServerApp
+application state pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
     msg <- WS.receiveData conn
     case T.split (== ' ') msg of
-        ["/join", room, user] -> handle user conn room logChan state
+        ["/join", room, user] -> handle user conn room state
         ["/list", room]       -> do
-            s <- readMVar state
+            s <- readTVarIO state
             WS.sendTextData
                 conn
                 ("Users: "
@@ -42,15 +43,9 @@ application logChan state pending = do
         _ -> WS.sendTextData conn ("Wrong announcement message." :: Text)
 
 
-handle
-    :: Text
-    -> WS.Connection
-    -> RoomName
-    -> UC.InChan Text
-    -> MVar ServerState
-    -> IO ()
-handle user conn room log clients = do
-    state <- readMVar clients
+handle :: Text -> WS.Connection -> RoomName -> TVar ServerState -> IO ()
+handle user conn room clients = do
+    state <- readTVarIO clients
     if
         | clientExists client room state -> WS.sendTextData
             conn
@@ -60,44 +55,43 @@ handle user conn room log clients = do
             ("Name cannot contain punctuation or whitespace and can't be empty" :: Text
             )
         | otherwise -> flip finally disconnect $ do
-            modifyMVar_ clients $ \s -> pure $ joinRoom client room s
-            s <- readMVar clients
+            s <- atomically $ do
+                modifyTVar' clients $ \s -> joinRoom client room s
+                readTVar clients
             WS.sendTextData conn $ "Welcome! users: " <> T.intercalate
                 ", "
                 (fromJust $ getUsernames room s)
-            broadcast log (fst client <> " joined " <> room) room s
-            talk log client room clients
+            broadcast (fst client <> " joined " <> room) room s
+            talk client room clients
   where
     client = (user, conn)
     isFormatted =
         any ($ fst client) [T.null, T.any isPunctuation, T.any isSpace]
     disconnect = do
-        s <- modifyMVar clients $ \s -> do
-            let s' = removeClient client room s
-            pure (s', s')
-        broadcast log (fst client <> " disconnected") room s
+        s <- atomically $ do
+            modifyTVar' clients $ \s -> removeClient client room s
+            readTVar clients
+        broadcast (fst client <> " disconnected") room s
 
-talk :: UC.InChan Text -> Client -> RoomName -> MVar ServerState -> IO ()
-talk logChan (user, conn) room state = forever $ do
+talk :: Client -> RoomName -> TVar ServerState -> IO ()
+talk (user, conn) room state = forever $ do
     msg <- WS.receiveData conn
     case T.split (== ' ') msg of
-        ["/join", newroom] -> quit >> handle user conn newroom logChan state
+        ["/join", newroom] -> quit >> handle user conn newroom state
         ["/leave"]         -> quit
         _                  -> do
-            s <- readMVar state
-            broadcast logChan (user <> ": " <> msg) room s
+            s <- readTVarIO state
+            broadcast (user <> ": " <> msg) room s
   where
     quit = do
-        s <- modifyMVar state $ \s -> do
-            let s' = removeClient (user, conn) room s
-            pure (s', s')
-        broadcast logChan (user <> " left room " <> room) room s
+        s <- atomically $ do
+            modifyTVar' state $ \s -> removeClient (user, conn) room s
+            readTVar state
+        broadcast (user <> " left room " <> room) room s
 
-broadcast :: UC.InChan Text -> Text -> RoomName -> ServerState -> IO ()
-broadcast logChan msg room state = do
-    -- T.putStrLn msg
+broadcast :: Text -> RoomName -> ServerState -> IO ()
+broadcast msg room state = do
     let clients = getClients room state
-    -- UC.writeChan logChan msg
     T.putStrLn msg
     case clients of
         Just members ->
@@ -106,14 +100,14 @@ broadcast logChan msg room state = do
 
 main :: IO ()
 main = do
-    state         <- newMVar newServerState
-    (logChan, rx) <- UC.newChan -- chan for logging
-    _             <- async $ forever $ do
-        msg <- UC.readChan rx
-        T.putStrLn $ "log: " <> msg
+    state <- atomically $ newTVar newServerState
+    -- (tx, rx) <- UC.newChan -- chan for logging
+    -- _             <- async $ forever $ do
+    --     msg <- UC.readChan rx
+    --     T.putStrLn $ "log: " <> msg
     run
         3000
         (WaiWS.websocketsOr WS.defaultConnectionOptions
-                            (application logChan state)
+                            (application state)
                             undefined
         )
